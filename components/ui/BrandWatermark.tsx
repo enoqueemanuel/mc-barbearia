883e001533b24d0fea232677c911b7ba480cc6ee
Enoque Macedo <enoqueemanuel8@gmail.com>
2026-09-24 21:40:14 -0300

    Centraliza marcas dagua e otimiza icone da guia

--- components/ui/BrandWatermark.tsx
+++ components/ui/BrandWatermark.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export function BrandWatermark() {
   return (
-    <div aria-hidden="true" className="pointer-events-none absolute -right-8 top-8 w-64 select-none opacity-[0.12] mix-blend-screen sm:right-8 sm:w-80">
+    <div aria-hidden="true" className="pointer-events-none absolute left-1/2 top-8 w-64 -translate-x-1/2 select-none opacity-[0.12] mix-blend-screen sm:w-80">
       <Image src="/brand-logo.png" alt="" width={512} height={512} className="h-auto w-full" />
     </div>
   );
